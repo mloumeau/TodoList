eaf287e19daeafacb97722058f68cf9b9d2682cd
mloumeau <matthewloumeau@gmail.com>
2022-01-21 12:12:17 -0700

    Created Todo List in Angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { TodosComponent } from './components/todos/todos.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodosComponent
+  ],
+  imports: [
+    FormsModule,
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
